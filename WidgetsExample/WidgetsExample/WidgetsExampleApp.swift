a30521851c4479ba663d4e5f6568e3c082356f70
Santiago Avila <santiago.avila@soundcloud.com>
2023-06-26 11:02:01 +0200

    Initial commit

---
+++ WidgetsExample/WidgetsExample/WidgetsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsExampleApp.swift
+//  WidgetsExample
+//
+//  Created by Santiago Avila on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WidgetsExample/WidgetsExtension/WidgetsExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsExtensionBundle.swift
+//  WidgetsExtension
+//
+//  Created by Santiago Avila on 26.06.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetsExtension()
+    }
+}
